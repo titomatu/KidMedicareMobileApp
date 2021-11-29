dbeebc152c3ff64f46873fcb82d2423445afe6a1
Tito Andrés Maturana <tamatu@gmail.com>
2021-11-29 12:15:05 -0500

    location map

--- app/src/main/java/org/tripulantesg2/kidmedicare/view/ui/home/HomeFragment.java
+++ app/src/main/java/org/tripulantesg2/kidmedicare/view/ui/home/HomeFragment.java
@@ -1,13 +1,24 @@
 package org.tripulantesg2.kidmedicare.view.ui.home;
 
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -22,10 +33,15 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import org.tripulantesg2.kidmedicare.R;
 import org.tripulantesg2.kidmedicare.databinding.FragmentHomeBinding;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements LocationListener {
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
+    private GoogleMap mapa;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+    private static final String TAG = "tester";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -35,43 +51,51 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        /*final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });*/
-        //Initialize Map Fragment region
+        locationManager = (LocationManager) getActivity().getSystemService(getContext().LOCATION_SERVICE);
+
+        ActivityResultLauncher<String[]> locationPermissionRequest =
+                registerForActivityResult(new ActivityResultContracts
+                                .RequestMultiplePermissions(), result -> {
+                            Boolean fineLocationGranted = result.getOrDefault(
+                                    Manifest.permission.ACCESS_FINE_LOCATION, false);
+                            Boolean coarseLocationGranted = result.getOrDefault(
+                                    Manifest.permission.ACCESS_COARSE_LOCATION,false);
+                            if (fineLocationGranted != null && fineLocationGranted) {
+                                // Precise location access granted.
+                                Log.w(TAG, "=============> permisions granted");
+                            } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                                // Only approximate location access granted.
+                                Log.w(TAG, "=============> one permisions");
+                            } else {
+                                Log.w(TAG, "=============> no permisions");
+                            }
+                        }
+                );
+
+        // ...
+
+        // Before you perform the actual permission request, check whether your app
+        // already has the permissions, and whether your app needs to show a permission
+        // rationale dialog. For more details, see Request permis
+
+        locationPermissionRequest.launch(new String[] {
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        });
+
+
         SupportMapFragment supportMapFragment = (SupportMapFragment)
-                getChildFragmentManager().findFragmentById(R.id.google_map );
+                getChildFragmentManager().findFragmentById(R.id.google_map);
         //Async Map
         supportMapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(@NonNull GoogleMap googleMap) {
-                //when Map is loaded
-                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-                    @Override
-                    public void onMapClick(@NonNull LatLng latLng) {
-                        //When clicked on map
-                        //Initialize markers options
-                        MarkerOptions markerOptions = new MarkerOptions();
-                        markerOptions.position(latLng);
-                        //Set Tittle of marker
-                        markerOptions.title(latLng.latitude + " : " + latLng.longitude);
-                        //Remove all markers
-                        googleMap.clear();
-                        //Animating the zoom the marker
-                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
-                                latLng, 10
-                        ));
-                        //Add marker on map
-                        googleMap.addMarker(markerOptions);
-                    }
-                });
+                mapa = googleMap;
+
+                requestMyLocation();
             }
         });
-        //end region
+
         return root;
     }
 
@@ -79,5 +103,69 @@ public class HomeFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    public void setMyLocation(LatLng latLng) {
+
+        //When clicked on map
+        //Initialize markers options
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        //Set Tittle of marker
+        markerOptions.title(latLng.latitude + " : " + latLng.longitude);
+        //Remove all markers
+        //mapa.clear();
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mapa.setMyLocationEnabled(true);
+        //Animating the zoom the marker
+        mapa.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                latLng, 10
+        ));
+        //Add marker on map
+        mapa.addMarker(markerOptions);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        setMyLocation(new LatLng(location.getLatitude(), location.getLongitude()));
+        Log.d(TAG, "==========> " + location.toString());
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.d("Latitude", "disable");
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.d("Latitude", "enable");
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d("Latitude", "status");
+    }
+
+    private void requestMyLocation() {
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
     }
 }
