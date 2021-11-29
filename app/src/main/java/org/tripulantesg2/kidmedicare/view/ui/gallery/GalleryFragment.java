c3f254d1cf1987fb621afb63cdb9cb58ac30b6a3
Tito Andrés Maturana <tamatu@gmail.com>
2021-11-29 08:03:43 -0500

    Fragmentos

--- app/src/main/java/org/tripulantesg2/kidmedicare/view/ui/gallery/GalleryFragment.java
+++ app/src/main/java/org/tripulantesg2/kidmedicare/view/ui/gallery/GalleryFragment.java
@@ -1,9 +1,13 @@
 package org.tripulantesg2.kidmedicare.view.ui.gallery;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,37 +16,53 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import org.tripulantesg2.kidmedicare.R;
 import org.tripulantesg2.kidmedicare.databinding.FragmentGalleryBinding;
+import org.tripulantesg2.kidmedicare.model.Solicitud;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GalleryFragment extends Fragment {
 
     private GalleryViewModel galleryViewModel;
     private FragmentGalleryBinding binding;
+    private static final String TAG = "tester";
 
-    private DatabaseReference mDatabase;
+    private FirebaseFirestore db;
+    private List<Solicitud> lista_solicitudes;
+    private ArrayAdapter<Solicitud> adapter;
+    private View root;
+
+    private AdapterView.OnItemClickListener messageClickedHandler = new AdapterView.OnItemClickListener() {
+        public void onItemClick(AdapterView parent, View v, int position, long id) {
+            Log.d(TAG, "Se seleccionó: " + lista_solicitudes.get(position).toString());
+        }
+    };
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        db = FirebaseFirestore.getInstance();
+        lista_solicitudes = new ArrayList<Solicitud>();
+
+        this.obtenerSolicitudes();
+
 
         galleryViewModel =
                 new ViewModelProvider(this).get(GalleryViewModel.class);
 
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        root = binding.getRoot();
 
-        final TextView textView = binding.textGallery;
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         return root;
     }
 
@@ -50,5 +70,36 @@ public class GalleryFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void obtenerSolicitudes(){
+        db.collection("solicitudes_kidmedicare")
+                .whereEqualTo("state", "A")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                Solicitud solicitud = document.toObject(Solicitud.class);
+                                lista_solicitudes.add(solicitud);
+                                Log.d(TAG, "===> documento: " + solicitud.toString());
+                            }
+
+                            adapter = new ArrayAdapter<Solicitud>(getContext(), android.R.layout.simple_list_item_1, lista_solicitudes);
+                            showListView(adapter);
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+
+    private void showListView(ArrayAdapter<Solicitud> adapter){
+        ListView listaSolicitudes = (ListView) root.findViewById(R.id.id_lista_solicitudes);
+
+        listaSolicitudes.setAdapter(adapter);
+        listaSolicitudes.setOnItemClickListener(messageClickedHandler);
     }
 }
